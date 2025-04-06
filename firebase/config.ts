9c7a42c02b0eb3a510f0889ef24b8d5c71858b85
kinumax <hidekiiinuma753@gmail.com>
2025-04-07 00:59:02 +0900

    Update config.ts

--- firebase/config.ts
+++ firebase/config.ts
@@ -1,13 +1,15 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+  apiKey: "AIzaSyC00EhOCnVW5yo1lC_nHR5J89Bk_N4nGjA",
+  authDomain: "roomx75.firebaseapp.com",
+  projectId: "roomx75",
+  storageBucket: "roomx75.firebasestorage.app",
+  messagingSenderId: "30346383945",
+  appId: "1:30346383945:web:dfa71ed6530c869381e598",
+  measurementId: "G-3RL9HL19N7"
 };
 
 const app = initializeApp(firebaseConfig);
